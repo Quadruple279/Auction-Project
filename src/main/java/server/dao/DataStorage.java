67b70bb9b3d8e4aa3ed94789d858c298ce20ca2b
Quadruple279 <inyourcrib@gmail.com>
2026-04-15 09:06:56 +0700

    Chỉnh sửa DataStorage để chạy ServerApp

--- src/main/java/server/dao/DataStorage.java
+++ src/main/java/server/dao/DataStorage.java
@@ -3,12 +3,12 @@ package server.dao;
 public class DataStorage {
     // Phải có phương thức loadData() (và các phương thức khác) để ServerApp.java dùng.
     // Load dữ liệu từ file (tạm thời để rỗng)
-    public void loadData() {
+    public static void loadData() {
         System.out.println("Loading data...");
     }
 
     // Lưu dữ liệu vào file (tạm thời để rỗng)
-    public void saveData() {
+    public static void saveData() {
         System.out.println("Saving data...");
     }
 }
